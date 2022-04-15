15a04d1266dc0ad67a7632dedcac3d4bdcb59d29
Bela4321 <93768776+Bela4321@users.noreply.github.com>
2022-04-15 15:49:16 +0200

    Aufgabe 1

--- project/src/Zettel01Bela/SortTools.java
+++ project/src/Zettel01Bela/SortTools.java
@@ -1,5 +1,7 @@
 package Zettel01Bela;
 
+import java.util.Comparator;
+
 public class SortTools {
     public static int[] createSequenceInc(int n) {
         int[] seq = new int[n];
@@ -45,28 +47,54 @@ public class SortTools {
         }
     }
 
+    public static <T extends Comparable> void insertionSortGen(T[] a) {
+        for (int i = 1; i < a.length; i++) {
+            int j = i;
+            while (j > 0 && a[j].compareTo(a[j - 1]) < 0) {
+                T tmp = a[j];
+                a[j] = a[j - 1];
+                a[j - 1] = tmp;
+                j--;
+            }
+        }
+    }
+
     public static void main(String[] args) {
         int testSize = 10;
-        int[] times=new int[testSize*5];
+        long[] times=new long[testSize*5];
         int[][] testData = { createSequenceDec(100), createSequenceDec(1000),createSequenceDec(10000), createSequenceDec(100000), createSequenceDec(200000)};
 
-        for (int i = 0; i < testData.length; i++) {
-            int[] temp = testData[i].clone();
-            for (int j = 0; j < testSize; j++) {
+        for (int j = 0; j < testSize; j++) {
+            for (int i = 0; i < testData.length; i++) {
+                int[] temp = testData[i].clone();
                 long start = System.nanoTime();
                 insertionSort(testData[i]);
-                times[i*testSize+j]=(int)(System.nanoTime()-start);
+                times[i*testSize+j]=(System.nanoTime()-start);
                 testData[i]=temp;
             }
         }
         for (int i = 0; i < testData.length; i++) {
             System.out.print("Test "+i+": ");
+            double avg=0;
             for (int j = 0; j < testSize; j++) {
                 System.out.printf("%5f ",times[i*testSize+j]*Math.pow(10,-9));
+                avg+=times[i*testSize+j]*Math.pow(10,-9);
             }
             System.out.println();
-
+            System.out.printf("\tAverage: %5f\n",avg/testSize);
         }
     }
+    /*results:
+    Test 0: 0,000150 0,000004 0,000004 0,000004 0,000004 0,000004 0,000004 0,000004 0,000005 0,000004
+	    Average: 0,000019
+    Test 1: 0,003340 0,000299 0,000308 0,000308 0,000308 0,000308 0,000299 0,000307 0,000308 0,000308
+	    Average: 0,000609
+    Test 2: 0,036265 0,029634 0,030261 0,030034 0,029404 0,030132 0,029416 0,030239 0,030275 0,030188
+	    Average: 0,030585
+    Test 3: 3,404859 2,993242 3,004208 3,008037 3,010638 3,014281 3,183810 3,013835 3,015538 3,003034
+	    Average: 3,065148
+    Test 4: 12,373809 12,175191 12,194604 12,190444 12,207006 12,439503 12,281419 12,241610 12,197787 12,202462
+	    Average: 12,250383
+     */
 
 }
