76ac04748e8e2312a933b6be53830e77ba849d89
franzi2112 <franziskasitek@gmail.com>
2022-05-19 10:07:17 +0200

    Ich glaube jetzt habe ich alle Methoden, erstmal überlegen müssen wie das nochmal geht mit den Interfaces

---
+++ project/src/Zettel05Franzi/PriorityQueueMinHeap.java
@@ -0,0 +1,78 @@
+package Zettel05Franzi;
+
+public class PriorityQueueMinHeap implements PriorityQueue<Integer> {
+    private int[] a;
+    private int heapsize;
+
+    public void decreaseKey(int i, int key){
+        if (key > a[i]){
+            throw new IllegalArgumentException("New key is bigger than old key.");
+        }
+        a[i] = key;
+        while(i > 1 && a[(i-1)/2] > a[i] ){
+            int dad = a[(i-1/2)];
+            a[i-1/2] = a[i];
+            a[i] = dad;
+            i = i-1/2;
+        }
+    }
+
+    public void minHeapify(int i){
+        int l = 2*i+1;
+        int r = 2*i+2;
+        int min;
+        if(l < heapsize && a[l] < a[i]){
+            min = l;
+        } else {
+            min = i;
+        } if ( r < heapsize && a[r] < a[min]){
+            min = r;
+        } if (min != i){
+            int temp = a[i];
+            a[i] = a[min];
+            a[min] = temp;
+            minHeapify(min);
+        }
+    }
+    public void buildMinHeap(){
+        for(int i = heapsize/2; i>=0; i--){
+            minHeapify(i);
+        }
+    }
+
+    public PriorityQueueMinHeap(int capacity){
+        a = new int[capacity];
+        heapsize = 0;
+        //warum hier nicht wirklich die Lnge also wüsste gerade auch nicht wie ich die ausrechnen sollte aber kann man die nicht ablesen?
+
+    }
+
+
+    public void addElement(Integer elem){
+        if(heapsize >= a.length){
+            throw new IllegalArgumentException("The Queue is already full!");
+        }
+        heapsize = heapsize + 1; //warum hast du das erst am Ende, aklso setzt du dann nicht bei dem momentanen Ende einfach einen neuen key ein?
+        a[heapsize] = Integer.MAX_VALUE;
+        decreaseKey(heapsize, elem);
+
+        
+    }
+
+    @Override
+    public Integer getFirst() {
+        return a[0];
+    }
+
+    @Override
+    public void deleteFirst() {
+        if (heapsize < 1){
+            throw new IllegalArgumentException("There are no keys!");
+        }
+        int min = a[0];
+        a[0] = a[heapsize];
+        heapsize--;
+        minHeapify(0);
+
+    }
+}
