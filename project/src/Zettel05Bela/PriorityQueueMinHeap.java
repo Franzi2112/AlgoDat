283dd55ba00916ce64f5f9d5ad36f0a86a74d00b
Bela4321 <93768776+Bela4321@users.noreply.github.com>
2022-05-20 11:32:18 +0200

    made Generic

--- project/src/Zettel05Bela/PriorityQueueMinHeap.java
+++ project/src/Zettel05Bela/PriorityQueueMinHeap.java
@@ -1,19 +1,20 @@
 package Zettel05Bela;
 
-public class PriorityQueueMinHeap implements PriorityQueue<Integer> {
-    private int[] a;
-    private int heapsize;
+import java.util.ArrayList;
+
+public class PriorityQueueMinHeap<T extends Comparable<T>> implements PriorityQueue<T> {
+    private ArrayList<T> a;
 
 
-    public void decreaseKey(int i, int key){
-        if(key>a[i]){
+    public void decreaseKey(int i, T key){
+        if(key.compareTo(a.get(i))>0){
             throw new IllegalArgumentException("New key is larger than current key");
         }
-        a[i]=key;
-        while(i>0 && a[(i-1)/2]>a[i]){
-            int temp=a[i];
-            a[i]=a[(i-1)/2];
-            a[(i-1)/2]=temp;
+        a.set(i,key);
+        while(i>0 && a.get((i-1)/2).compareTo(a.get(i))>0){
+            T temp=a.get(i);
+            a.set(i,a.get((i-1)/2));
+            a.set((i-1)/2,temp);
             i=(i-1)/2;
         }
     }
@@ -24,52 +25,42 @@ public class PriorityQueueMinHeap implements PriorityQueue<Integer> {
         int l=2*i+1;
         int r=2*i+2;
         int smallest;
-        if(l<heapsize && a[l]<a[i]){
+        if(l<a.size() && a.get(l).compareTo(a.get(i))<0){
             smallest=l;
         } else {
             smallest=i;
         }
-        if(r<heapsize && a[r]<a[smallest]){
+        if(r<a.size() && a.get(r).compareTo(a.get(smallest))<0){
             smallest=r;
         }
         if(smallest!=i){
-            int temp=a[i];
-            a[i]=a[smallest];
-            a[smallest]=temp;
+            T temp=a.get(i);
+            a.set(i,a.get(smallest));
+            a.set(smallest,temp);
             minHeapify(smallest);
         }
     }
 
     private void buildMinHeap() {
-        for(int i=heapsize/2;i>=0;i--){
+        for(int i=a.size()/2;i>=0;i--){
             minHeapify(i);
         }
     }
 
-    public PriorityQueueMinHeap(int capacity) {
-        a=new int[capacity];
-        heapsize=0;
+    @Override
+    public void addElement(T elem) {
+        a.add(elem);
+        decreaseKey(a.size(),elem);
     }
 
     @Override
-    public void addElement(Integer elem) {
-        if(heapsize>=a.length){
-            throw new IllegalStateException("PriorityQueue is full");
-        }
-        a[heapsize]=Integer.MAX_VALUE;
-        decreaseKey(heapsize,elem);
-        heapsize++;
-    }
-
-    @Override
-    public Integer getFirst() {
-        return a[0];
+    public T getFirst() {
+        return a.get(0);
     }
 
     @Override
     public void deleteFirst() {
-        a[0]=a[heapsize-1];
-        heapsize--;
+        a.set(0,a.get(a.size()-1));
         minHeapify(0);
     }
 }
