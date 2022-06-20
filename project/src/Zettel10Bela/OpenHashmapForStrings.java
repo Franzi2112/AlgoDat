d7a2d3202f77011df6b2a3a727a6c1d488bd625f
Bela <bela.schinke@gmail.com>
2022-06-20 19:10:20 +0200

    Update/working

--- project/src/Zettel10Bela/OpenHashmapForStrings.java
+++ project/src/Zettel10Bela/OpenHashmapForStrings.java
@@ -5,91 +5,131 @@ import Zettel09Bela.HashmapForStrings;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 
 public class OpenHashmapForStrings {
+    Deleted deleted= new Deleted();
     int size; //=m
     Object[] hashtable;
-    int selectedHashFunction;
+    BiFunction<String, Integer, Integer> selectedHashFunction;
 
-    public OpenHashmapForStrings(int m) {
+    public OpenHashmapForStrings(int m, String mode) {
         this.size = m;
         this.hashtable = new String[m];
+        if (mode =="linear"){
+            selectedHashFunction=this::linearHash;
+        } else if (mode=="quadratic") {
+            selectedHashFunction=this::quadraticHash;
+        } else if (mode=="double") {
+            selectedHashFunction=this::doubleHash;
+        } else {
+            throw new IllegalArgumentException("not a valid Hashtype");
+        }
+    }
+
+    private int stringToNumber(String string) {
+        int sum=1;
+        for (char c : string.toCharArray()) {
+            sum *=c;
+        }
+        return Math.abs(sum);
     }
 
     public int hashHelp(String string) {
-        byte[] bytes= string.getBytes();
-        int sum=0;
-        int counter=0;
-        for (byte bite:bytes) {
-            sum+= Byte.toUnsignedInt(bite)*counter++;
-        }
-        return sum%this.size;
+        return stringToNumber(string)%this.size;
     }
 
     public int linearHash(String string, int i) {
-        return (hashHelp(string)+i)%this.size;
+        return (stringToNumber(string)+i)%this.size;
     }
 
     public int quadraticHash(String string, int i) {
-        return (hashHelp(string)+i/2+i*i/2)%this.size;
+        return (stringToNumber(string)+i/2+i*i/2)%this.size;
     }
 
     private int hash2(String string) {
-        byte[] bytes= string.getBytes();
-        int sum=0;
-        int counter=0;
-        for (byte bite:bytes) {
-            sum+= Byte.toUnsignedInt(bite)*counter++;
-        }
-        return 1+(sum%(this.size-1));
+        int hash= stringToNumber(string);
+        while ((1+hash%(this.size-1))%5==0||(1+hash%(this.size-1))%2==0){hash++;}
+        return Math.abs(1+hash%(this.size-1));
     }
 
     public int doubleHash(String string, int i) {
-        return (hashHelp(string)+i*hash2(string))%this.size;
+        return Math.abs((stringToNumber(string)+i*hash2(string)%this.size)%this.size);
     }
 
     public void insert(String string) {
-        int hash= hash(string);
-        this.hashtable[hash].add(string);
+        int i =0;
+        while (i<size){
+            int hash = selectedHashFunction.apply(string, i++);
+
+            if (this.hashtable[hash]==null||this.hashtable[hash]==deleted){
+                this.hashtable[hash]=string;
+                return;
+            }
+            if (this.hashtable[hash].equals(string)){
+                return;
+            }
+        }
+        throw new OutOfMemoryError("hashtable is full");
     }
 
     public void delete(String string) {
-        int hash =hash(string);
-        while (this.hashtable[hash].remove(string)){}
+        int i=0;
+        while (i<size) {
+            int hash = this.selectedHashFunction.apply(string, i++);
+            if (this.hashtable[hash]==string){
+                this.hashtable[hash] = deleted;
+                return;
+            }
+        }
     }
 
     public boolean search(String string) {
-        int hash = hash(string);
-        return this.hashtable[hash].contains(string);
+        int i = 0;
+        while (i<size) {
+            int hash =selectedHashFunction.apply(string, i++);
+            if (this.hashtable[hash]==null) {
+                return false;
+            }else if (this.hashtable[hash].equals(string)){
+                return true;
+            }
+        }
+        return false;
     }
 
     public static void main(String[] args) throws Exception{
-        int[] sizes = {200000,1000000};
+        int[] sizes = {200000, 1000000};
+        String[] modes = {"linear", "quadratic", "double"};
 
-        for (int size: sizes) {
-            HashmapForStrings map = new HashmapForStrings(1000);
-            FileReader scrabble = new FileReader("project/src/Zettel09Bela/OfficialScrabbleWordListGerman.txt");
-            FileReader friends = new FileReader("project/src/Zettel09Bela/AreMyFriendsCheating.txt");
+        for (String mode: modes) {
+            System.out.println("\nhashfunction: "+ mode);
+            for (int size: sizes) {
 
-            System.out.println("Tablesize: "+size);
+                OpenHashmapForStrings map = new OpenHashmapForStrings(size,mode);
+                FileReader scrabble = new FileReader("project/src/Zettel09Bela/OfficialScrabbleWordListGerman.txt");
+                FileReader friends = new FileReader("project/src/Zettel09Bela/AreMyFriendsCheating.txt");
 
-            BufferedReader reader = new BufferedReader(scrabble);
-            long currentTime= System.nanoTime();
-            reader.lines().forEach((String str) ->map.insert(str));
-            long time = System.nanoTime()-currentTime;
-            System.out.println("\tInsertion Time: "+time*Math.pow(10,-9));
+                System.out.println("Tablesize: "+size);
+
+                BufferedReader reader = new BufferedReader(scrabble);
+                long currentTime= System.nanoTime();
+                reader.lines().forEach((String str) ->map.insert(str));
+                long time = System.nanoTime()-currentTime;
+                System.out.println("\tInsertion Time: "+time*Math.pow(10,-9));
 
 
-            ContainsCheck containsCheck = new ContainsCheck();
+                ContainsCheck containsCheck = new ContainsCheck();
 
-            reader = new BufferedReader(friends);
-            int counter=0;
-            currentTime= System.nanoTime();
-            reader.lines().forEach((String s)->containsCheck.accept(s,map));
-            time = System.nanoTime()-currentTime;
-            System.out.println("\tSearch Time: "+time*Math.pow(10,-9));
-            System.out.println("\tErfolglose Suchen: "+containsCheck.count);
+                reader = new BufferedReader(friends);
+                int counter=0;
+                currentTime= System.nanoTime();
+                reader.lines().forEach((String s)->containsCheck.accept(s,map));
+                time = System.nanoTime()-currentTime;
+                System.out.println("\tSearch Time: "+time*Math.pow(10,-9));
+                System.out.println("\tErfolglose Suchen: "+containsCheck.count);
 
+            }
         }
     }
 
@@ -99,12 +139,44 @@ public class OpenHashmapForStrings {
 class Deleted {}
 
 
-class ContainsCheck implements BiConsumer<String, HashmapForStrings> {
+class ContainsCheck implements BiConsumer<String, OpenHashmapForStrings> {
     int count=0;
     @Override
-    public void accept(String s,HashmapForStrings map) {
+    public void accept(String s,OpenHashmapForStrings map) {
         if (!map.search(s)){
             count++;
         }
     }
 }
+/*
+
+hashfunction: linear
+Tablesize: 200000
+	Insertion Time: 0.40906970000000004
+	Search Time: 0.0488859
+	Erfolglose Suchen: 1814
+Tablesize: 1000000
+	Insertion Time: 0.1227223
+	Search Time: 0.0027151000000000002
+	Erfolglose Suchen: 1814
+
+hashfunction: quadratic
+Tablesize: 200000
+	Insertion Time: 0.44208010000000003
+	Search Time: 0.026723100000000003
+	Erfolglose Suchen: 1814
+Tablesize: 1000000
+	Insertion Time: 0.12181220000000001
+	Search Time: 0.0027604
+	Erfolglose Suchen: 1814
+
+hashfunction: double
+Tablesize: 200000
+	Insertion Time: 0.2642795
+	Search Time: 0.0090028
+	Erfolglose Suchen: 1814
+Tablesize: 1000000
+	Insertion Time: 0.0891531
+	Search Time: 0.002532
+	Erfolglose Suchen: 1814
+ */
