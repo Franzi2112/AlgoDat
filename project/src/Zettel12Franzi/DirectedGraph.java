281950e94186770134cd1032fa5b34e1a856e5aa
franzi2112 <franziskasitek@gmail.com>
2022-07-07 11:48:58 +0200

    Habe es glaube geschafft was ich wollte

--- project/src/Zettel12Franzi/DirectedGraph.java
+++ project/src/Zettel12Franzi/DirectedGraph.java
@@ -1,42 +1,38 @@
 package Zettel12Franzi;
 
+import Zettel11Franzi.UndirectedGraph;
+
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.sql.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.stream.Stream;
+import java.util.*;
+import java.util.stream.IntStream;
 
 public class DirectedGraph {
     List<Integer> Nodes;
     HashMap<Integer, List<Integer>> AL;
 
-    public DirectedGraph(){
+    public DirectedGraph() {
         Nodes = new ArrayList<>();
         AL = new HashMap<>();
     }
 
-    public DirectedGraph(Integer n){
+    public DirectedGraph(Integer n) {
         Nodes = new ArrayList<>();
         AL = new HashMap<>();
-        for(int i = 0; i < n; i++){
-            Nodes.add(i+1);
+        for (int i = 0; i < n; i++) {
+            Nodes.add(i + 1);
         }
     }
 
-    public void addVertex(Integer i){
-        if(Nodes.contains(i)){
+    public void addVertex(Integer i) {
+        if (Nodes.contains(i)) {
             return;
         } else {
             Nodes.add(i);
         }
     }
 
-    public void addEdge(Integer i, Integer j){
-        if (!AL.containsKey(i)){
+    public void addEdge(Integer i, Integer j) {
+        if (!AL.containsKey(i)) {
             AL.put(i, new ArrayList<>());
             AL.get(i).add(j);
         } else {
@@ -44,16 +40,40 @@ public class DirectedGraph {
         }
     }
 
-    public void deleteEdge(Integer i, Integer j){
+    public void deleteEdge(Integer i, Integer j) {
         AL.get(i).remove(j);
     }
 
-    public void fileToGraph(String filename) throws IOException {
-        Stream<String> lines = Files.lines(Path.of(filename)).map(x -> (x.split(" ").
-
-            }
+    public static DirectedGraph[] fileToGraph(String filename) throws Exception {
+        DirectedGraph Graph = new DirectedGraph();
+        List<String> listOfStrings = new ArrayList<String>();
+        BufferedReader bf = new BufferedReader(new FileReader(filename));
+        String line = bf.readLine();
+        while (line != null) {
+            listOfStrings.add(line);
+            line = bf.readLine();
         }
+        bf.close();
+        String[] array = listOfStrings.toArray(new String[0]);
+        for (String str : array) {
+            int[] alr = Arrays.stream(str.split(" ")).flatMapToInt(x -> IntStream.of(Integer.parseInt(x))).toArray();
+            Graph.addEdge(alr[0], alr[1]);
+        }
+        return new DirectedGraph[]{Graph};
     }
 
+    public LinkedList<Integer> topSort(DirectedGraph)
 
+
+    public static void main(String[] args) throws Exception {
+        DirectedGraph[] outbnet = fileToGraph("project/src/Zettel12Franzi/outbnet.sec");
+        DirectedGraph[] outmaayan = fileToGraph("project/src/Zettel12Franzi/outmaayan-figeys.sec");
+        DirectedGraph[] outmorenoDense = fileToGraph("project/src/Zettel12Franzi/outmoreno_dense_comm.sec");
+        DirectedGraph[] outmorenoTaro = fileToGraph("project/src/Zettel12Franzi/outmoreno_taro_taro.sec");
+        DirectedGraph[] outsimple = fileToGraph("project/src/Zettel12Franzi/outsimple.sec");
+
+    }
 }
+
+
+
